bb5227f33c034f056e11c9b87118e26e44482974
iamromanh <romanh99@gmail.com>
2016-11-26 02:02:04 -0500

    add event listener & wordcount modification function

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+/*jshint esversion: 6 */
+
+// Single state object
 $(document).ready(function(){
 
-  let state = (test, faction) => {
-    console.log('test');
-  };
+  // let storage = (k, v) => {
+  //     totalWord: 0,
+  //     uniqueword: 0,
+  //     averageword: 0,
+  //     averageSentence:
+  // }
 
+// Event listeners
+  $('.js-text-form').on('submit', function(event){
+    event.preventDefault();
+    const formInput = $('#user-text').val();
+    wordCount(formInput);
+  });
 });
+
+
+// State modification functions
+let wordCount = (text) => {
+  alert(`I was transfered from the event listener here is the message: ${text}`);
+  let splitMessage = text.split();
+  console.log(splitMessage);
+};
