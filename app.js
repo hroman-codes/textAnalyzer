3e8963dfda60afaf98be14eef58ad8a5d606800d
iamromanh <romanh99@gmail.com>
2016-11-29 00:26:49 -0500

    add uniqueWord function

--- app.js
+++ app.js
@@ -2,34 +2,47 @@
 
 // Single state object
 $(document).ready(function(){
+  let state = {
+    totalWord: 0,
+    uniqueword: 0,
+    //     averageword: 0,
+    //     averageSentence:
+  };
 
-  let state = (k, v) => {
-      totalWord: [];
-  //     uniqueword: 0,
-  //     averageword: 0,
-  //     averageSentence:
-};
-
-// Event listeners
+  // Event listeners
   $('.js-text-form').on('submit', function(event){
     event.preventDefault();
     const formInput = $('#user-text').val();
     wordCount(formInput);
+    uniqueWord(formInput);
   });
-});
-
 
 // State modification functions
-let wordCount = (text) => {
-  let splitMessage = text.split(" ");
-  // let wordCountArray = [];
-  for(var i = 0; i < splitMessage.length; i++){
-    state.totalWord.push(splitMessage[i]); // the intent it to push the wordcount to the state object to keep sepration of concerns.
+let wordCount = (formInput) => {
+  const splitMessage = formInput.split(" ");
+  alert(`we have ${splitMessage.length} words in our array`);
+  state.totalWord = splitMessage.length;
+  console.log(state.totalWord);
+
+};
+
+let uniqueWord = (formInput) => {
+  let uniqueWords = [];
+  const splitMessage = formInput.split(" ");
+  for (var i = 0; i < splitMessage.length; i++){
+        for (var j = 0; j < uniqueWords.length; j++){
+          if(splitMessage.length === uniqueWords.length) {
+            uniqueWords.push(splitMessage[i]);
+            console.log(uniqueWords);
+        {
+        }
+      }
+    }
   }
-  alert(`we have ${state.totalWord.length} words in our array`);
 };
 
 // Render functions
 
 
 // Init functions
+});
