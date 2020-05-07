3e2c24f74e0eed0063b820fee7247b46d41e6b17
getromandev <getroman.dev@gmail.com>
2020-05-06 20:25:36 -0400

    refactor render functions and event listeners

--- app.js
+++ app.js
@@ -4,55 +4,54 @@
 $(document).ready(function(){
   let state = {
     totalWord: 0,
-    uniqueword: 0,
-    //     averageword: 0,
-    //     averageSentence:
+    // uniqueword: 0,
+    // averageword: 0,
+    // averageSentence:
   };
+
+  // Render functions
+  let renderTotalWord = (state, element) => {
+    element.empty().append(`<span> ${state.totalWord} </span>`)
+  }
+
+
+  // State modification functions
+  let totalWord = (state, formInput) => {
+    let splitMessage = formInput.split(" ");
+    let filtered = splitMessage.filter(Boolean);
+
+    state.totalWord = filtered.length;
+  };
+
+
+// let uniqueWord = (formInput) => {
+//   let uniqueWords = [];
+//   const splitMessage = formInput.split(" "); // why am I doing this twice once in wordCount and uniqueWord function?
+//   // set a vairable to be the mechinism to hold the false value.
+//     for(i = 0; i < splitMessage.length; i++ ){
+//       let repeated = false;
+//       if (uniqueWords.length === 0) {
+//         uniqueWords.push(splitMessage[i]);
+//       }
+//     for(j = 0; j < uniqueWords.length; j++) {
+//       if (splitMessage[i] === uniqueWords[j]) {
+//         repeated = true;
+//         }
+//       }
+//       if (!repeated) {
+//         uniqueWords.push(splitMessage[i]);
+//       }
+//     }
+//     console.log(uniqueWords);
+//     return uniqueWords;
+// };
 
   // Event listeners
   $('.js-text-form').on('submit', function(event){
-    event.preventDefault();
-    // I added the replace method and passed in a regex, the intent was to remove the white space being counted as a word
-    // const formInput = $('#user-text').val().replace(/\s/g, '');
-    // the right way is to add the trim method to remove the white space from both ends of the string
     const formInput = $('#user-text').val().trim();
-    wordCount(formInput);
-    uniqueWord(formInput);
+    event.preventDefault();
+
+    totalWord(state, formInput);
+    renderTotalWord(state, $("#s1"));
   });
-
-// State modification functions
-let wordCount = (formInput) => {
-  const splitMessage = formInput.split(" ");
-  alert(`we have ${splitMessage.length} words in our array`);
-  state.totalWord = splitMessage.length;
-};
-
-
-let uniqueWord = (formInput) => {
-  let uniqueWords = [];
-  const splitMessage = formInput.split(" "); // why am I doing this twice once in wordCount and uniqueWord function?
-  // set a vairable to be the mechinism to hold the false value.
-    for(i = 0; i < splitMessage.length; i++ ){
-      let repeated = false;
-      if (uniqueWords.length === 0) {
-        uniqueWords.push(splitMessage[i]);
-      }
-    for(j = 0; j < uniqueWords.length; j++) {
-      if (splitMessage[i] === uniqueWords[j]) {
-        repeated = true;
-        }
-      }
-      if (!repeated) {
-        uniqueWords.push(splitMessage[i]);
-      }
-    }
-    console.log(uniqueWords);
-    return uniqueWords;
-};
-
-
-// Render functions
-
-
-// Init functions
 });
